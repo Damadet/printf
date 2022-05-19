bee516e704530fd4bca038d8aa4f779740caf0d0
Favour Ukeseh <favxzy@gmail.com>
2022-05-19 10:50:01 -0700

    Task 0 new commit

--- main.h
+++ main.h
@@ -1,46 +1,33 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
+
 /**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
 {
-	char *sym;
+	char specifiers;
 	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+} specifiers_t;
 
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+/*prototypes*/
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+int get_function(char s, va_list args);
+int _putchar(char c);
 
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
-
+/*Conversion specifiers*/
+int print_char(va_list args);
+int print_string(va_list args);
+int print_digit(va_list args);
+int print_mod(va_list args);
+int print_rev_string(va_list args);
 
 #endif
